a1a2c2bd5344f45f312e72f6c256a598c064843b
cal1801 <pike1801@gmail.com>
2018-01-08 14:22:39 -0500

    Update layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ActionController::Base
     @users = User.all
 
     @weight_data = {}
-    @users.each do |user|
+    @users.order(:id).each do |user|
       user_data = @weight_data[user.id] = {}
-      user.weight_data.each do |weight|
-        user_data[weight.created_at] = weight.weight
+
+      user.weight_data.group_by{|d| d.created_at.all_week}.each do |date, weights|
+        user_data[date.first.strftime("%b %d, %Y")] = weights.max_by(&:created_at).weight
       end
     end
   end
