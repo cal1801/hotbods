4b9c96a50177a6b363f2efbbd48f9ced991eba54
cal1801 <pike1801@gmail.com>
2018-01-09 09:41:18 -0500

    Add this week graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,21 @@ class HomeController < ActionController::Base
     @users = User.all
 
     @weight_data = {}
+
     @users.order(:id).each do |user|
       user_data = @weight_data[user.id] = {}
 
       user.weight_data.group_by{|d| d.created_at.all_week}.each do |date, weights|
         user_data[date.first.strftime("%b %d, %Y")] = weights.max_by(&:created_at).weight
+      end
+
+      @this_week = {}
+      @users.order(:id).each do |user|
+        data = @this_week[user.id] = {}
+        Date.today().all_week.each do |date|
+          weight = user.weight_data.select{|d| d.created_at.to_date == date}.first
+          data[date] = weight.weight unless weight.nil?
+        end
       end
     end
   end
